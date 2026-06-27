07007c87ef8c304d06f660aad3165e005342bea1
Adriel Ynfante <eliezerynfante3034@gmail.com>
2026-06-26 20:04:39 -0500

    feat(routes): add basic views for navigation links

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/nosotros', function () {
+    return view('nosotros');
+});
+
+Route::get('/carta', function () {
+    return view('carta');
+});
+
+Route::get('/reserva', function () {
+    return view('reserva');
+});
+
+Route::get('/galeria', function () {
+    return view('galeria');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
 Route::get('/login', function () {
-    return view('login');
+    return view('auth.login');
 });
 
